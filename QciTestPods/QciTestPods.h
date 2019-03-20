79766ac13a8cbd6ac9d901d902dbb6e5f24bd98a
Joseph <kanonzero0613@gmail.com>
2019-03-20 11:08:59 +0800

    Initial project setup

---
+++ QciTestPods/QciTestPods.h
@@ -0,0 +1,19 @@
+//
+//  QciTestPods.h
+//  QciTestPods
+//
+//  Created by Joseph on 2019/3/20.
+//  Copyright © 2019 Joseph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QciTestPods.
+FOUNDATION_EXPORT double QciTestPodsVersionNumber;
+
+//! Project version string for QciTestPods.
+FOUNDATION_EXPORT const unsigned char QciTestPodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QciTestPods/PublicHeader.h>
+
+
